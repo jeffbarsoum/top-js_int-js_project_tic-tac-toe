327dba7da63277bf6a46b9182ce3b830292f8bdc
jeffbarsoum <143226227+jeffbarsoum@users.noreply.github.com>
2025-03-18 20:56:55 -0400

    Add functionality to play multiple rounds, return overall winner and playCount

--- js/game.js
+++ js/game.js
@@ -1,18 +1,21 @@
 import { createGameBoard } from './gameBoard.js'
 import { createPlayers } from './player.js'
+import { cloneObj } from './cloneObj.js';
 
 const game = (
     () => {
         const gameBoard = createGameBoard;
         const players = createPlayers;
         const gameResults = {};
-        const wins = [];
+        // const wins = [];
 
-        let gameStartTime;
-        let playCount = 0;
+        // let gameStartTime;
+        // let playCount = 0;
         players.createPlayer('steve', 'x');
         players.createPlayer('alice', 'o');
-        console.log(players.getActivePlayers().forEach(player => console.log(player.getPieceId())))
+        console.log(players.getActivePlayers().forEach(player => console.log(player)))
+
+        // const addPlayCount = () => { return playCount++ }
 
 
         const playerTurn = () => {
@@ -24,27 +27,38 @@ const game = (
             }
             while (!turnResult) {
                 const emptySquares = `Empty squares: ${getEmptySquares.map((piece) => `[${piece.row}, ${piece.col}]`)}`;
-                const currentPlayer = players.getCurrentPlayer();
-                const name = currentPlayer.getName();
-                const pieceId = currentPlayer.getPieceId();
+                const currentPlayer = players.getCurrentPlayer(true);
                 // alert(msg)
 
                 turnResult = gameBoard.setPiece(
-                    name,
-                    pieceId,
-                    Number(prompt(`${emptySquares}\n\n${name}, please enter the row you want to place your piece:`)),
+                    currentPlayer.name,
+                    currentPlayer.pieceId,
+                    Number(prompt(`${emptySquares}\n\n${currentPlayer.name}, please enter the row you want to place your piece:`)),
                     Number(prompt(`${emptySquares}\n\nCool, now the column pls...:`))
                 )
-
                 
                 if (turnResult) break;
                 
                 alert('Hey, like, maybe select an empty square...');
             }   
-            players.nextPlayer();
         }
 
-        const play = (_wins = [], _gameResults = {}, _gameStartTime = new Date().getTime()) => {
+        const getWinner = () => {
+            let winningScore = 0;
+            let winner;
+            if (players.getActivePlayers().every((player, i, arr) => player.score === arr[0].score)) return 'draw';
+            console.log('getWinner launched - getActivePlayers()', players.getActivePlayers())
+            players.getActivePlayers().forEach(player => {
+                console.log('getWinner launced...player: ', player)
+                if(player.score > winningScore) {
+                    winningScore = player.score
+                    winner = player;
+                }
+            })
+            return winner;       
+        }
+
+        const play = (wins = [], playCount = 0, gameStartTime = new Date().getTime()) => {
             let win;
 
             while(!(win)) {
@@ -52,24 +66,38 @@ const game = (
                 win = gameBoard.isWin();
                 console.log('gameboard, check win....')
                 console.log(win)
+                if (!win) players.nextPlayer();
             }
 
-            players.getCurrentPlayer().addScore();
+
+            if(win.win !== 'draw') players.getCurrentPlayer(true).addScore();
+            // console.log('game.play = playCount before...: ', playCount)
+            playCount++;
+            // console.log('game.play = playCount after...: ', playCount)
             
             // add players with scores to gameBoard win result for saving
-            _wins.push({ ...win, players: players.getActivePlayers().map(player => player.getPlayer()) })
+            wins.push(win);
             win = null;
 
-            let playAgain = confirm(`${players.getCurrentPlayer().getName()} wins!\n\nDo you want to play again?`)
+            let playAgain = confirm(`${players.getCurrentPlayer().name} wins!\n\nDo you want to play again?`)
 
             if (playAgain) {
                 gameBoard.resetGame();
-                play(_wins, _gameResults, _gameStartTime);
-            } 
+                play(wins, playCount, gameStartTime);
+            } else {
 
-            _gameResults[_gameStartTime] = _wins
-            console.log('game ended, print gameResults...:', _gameResults)
-            return _gameResults
+                gameResults[gameStartTime] = { 
+                    wins: cloneObj(wins),
+                    playCount, 
+                    players: players.getActivePlayers(), 
+                    winner: getWinner()
+                }
+                wins.length = 0;
+                playCount = 0;
+                console.log('game ended, print gameResults...:', gameResults)
+                return gameResults
+            }
+
 
             // console.log('game ended, print matrix after external reset...:', gameBoard.matrix )
         }
